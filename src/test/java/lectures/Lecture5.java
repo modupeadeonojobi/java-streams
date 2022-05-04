58e7e4744f4d16b16cf5b30ca9abe97778e20145
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-05-04 11:16:41 +0100

    Lec05 Use map to transform data type

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -18,6 +18,8 @@ public class Lecture5 {
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
+    List<Car> carFiltered = cars.stream().filter(car -> car.getPrice() < 20000).limit(20).collect(Collectors.toList());
+    carFiltered.forEach(System.out::println);
 
   }
 
@@ -25,12 +27,17 @@ public class Lecture5 {
   public void ourFirstMapping() throws Exception {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
+    List<PersonDTO> personDTOS = people.stream().map(PersonDTO::map).collect(Collectors.toList());
+    personDTOS.forEach(System.out::println);
+
 
   }
 
   @Test
   public void averageCarPrice() throws Exception {
     // calculate average of car prices
+    ImmutableList<Car> cars = MockData.getCars();
+
 
   }
 
