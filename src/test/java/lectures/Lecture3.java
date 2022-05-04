adf2bf671d8f4bf0ff671f434baa8a15f30e8811
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-05-04 10:38:39 +0100

    Lec03 Get the min and max of a list

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -1,23 +1,26 @@
 package lectures;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
 import java.util.Comparator;
 import java.util.List;
-import org.junit.Test;
 
 public class Lecture3 {
 
   @Test
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    Integer minNumber = numbers.stream().min(Comparator.naturalOrder()).get();
+    System.out.println(minNumber);
 
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    Integer maxNumber = numbers.stream().max(Comparator.naturalOrder()).get();
+    System.out.println(maxNumber);
 
   }
 }
