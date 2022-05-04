7d9c11e9f1beac7dfbfdf4881169043e1e391b89
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-05-04 14:35:22 +0100

    Lec06 Add findAny() and findFirst() solution

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -1,10 +1,10 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.function.Predicate;
-import org.junit.Test;
 
 public class Lecture6 {
 
@@ -13,13 +13,16 @@ public class Lecture6 {
   @Test
   public void findAny() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Integer anyNum = Arrays.stream(numbers).filter(numbersLessThan10).findAny().get();
+    System.out.println(anyNum);
 
   }
 
   @Test
   public void findFirst() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+    Integer firstNum = Arrays.stream(numbers).filter(numbersLessThan10).findFirst().get();
+    System.out.println(firstNum);
   }
 }
 
