e986a49eef9cc907e9170bb6a6c32ca467bde7d2
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-05-09 16:23:17 +0100

    Lec09 Add reduce

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -1,15 +1,28 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
-import java.util.Arrays;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class Lecture9 {
 
   @Test
   public void reduce() throws Exception {
     Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+
+    // Integer::sum is same as .reduce(0, (a,b) -> a+b)
+    Integer sum = Arrays.stream(integers).reduce(0, Integer::sum);
+    System.out.println(sum);
+
+  }
+
+  @Test
+  public void stringReduce() throws Exception {
+    String[] string = {"a", "b", "c", "d", "e", "f", "g"};
+
+    String stringReduce = Arrays.stream(string).reduce("", String::concat);
+    System.out.println(stringReduce);
 
   }
 
